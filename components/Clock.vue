503a7292f1eec18f3fdc567b54efbcd8333622fe
Matt Cavanagh <maelstrome26@gmail.com>
2023-03-14 19:50:37 +0000

    Added seo stuffs

--- components/Clock.vue
+++ components/Clock.vue
@@ -19,6 +19,17 @@ const props = defineProps<{
   endDate: string
 }>()
 
+const description = 'This project is to track the amount of time between two love birds meeting each other next. It brings a whole new level to clock watching!'
+
+useServerSeoMeta({
+  title: 'CrazyClock',
+  ogTitle: 'CrazyClock',
+  description,
+  ogDescription: description,
+  ogImage: 'https://previews.123rf.com/images/kongvector/kongvector1708/kongvector170803424/84740660-crazy-clock-character-cartoon-style-vector-illustration.jpg', // Shameless copyright infringement
+  twitterCard: 'summary_large_image',
+})
+
 const barProps = ref({
   width: '400px'
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  }
 })
